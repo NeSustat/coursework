4fd2da09a1db70f92c3d8d20404a982787b24f35
NeSustat <nesustat0v@icloud.com>
2025-12-05 21:57:00 +0300

    designer

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <cmath>
+#include <string>
+#include <vector>
 
+// класс отвечающий за облако слов
 class WordCloud {
-    private:
-
-    public:
-    
+protected:
+    std::vector<std::string> words;
+public:
+    // конструктор. собирает вектор со словами разделяя их по пробелу
+    WordCloud(){
+        std::cout << "write word for word cloud\n";
+        std::string word;
+        while (std::cin >> word){
+            words.push_back(word);
+            if (std::cin.peek() == '\n'){
+                break;
+            }
+        }
+    }
 };
 
 int main(){
