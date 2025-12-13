550a63e7418de69270e7540e966c7e43eed87fca
NeSustat <nesustat0v@icloud.com>
2025-12-14 00:24:25 +0300

    3/5

--- struct.cpp
+++ struct.cpp
@@ -1,6 +1,6 @@
 #include "struct.h"
 
-WordCloud::WordCloud(){
+WordCloud::WordCloud(int side){
     std::cout << "write word for word cloud\n";
         std::string word;
         while (std::cin >> word){
@@ -9,11 +9,37 @@ WordCloud::WordCloud(){
                 break;
             }
         }
-        WordCloud::len = words.size();
+        WordCloud::sideCircle = side;
 }
 
 bool WordCloud::checkPlace(int x, int y, std::string& word, const QFont font){
+    int width, lenght;
+    QSize sizeWord = getSize(word, font);
+    width = sizeWord.width;
+    lenght = sizeWord.lenght;
+    if (x < 0 || y < 0 || x + lenght > 512 || y + wight > 512){
+        return false;
+    }
+    for (int i = x; i < x + lenght; i++){
+        for (int j = y; j < y + width; j++){
+            if (freePlase[i][j]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
+void switchFlag(int x, int y, const std::string& word, const QFont& font){
+    int width, lenght;
+    QSize sizeWord = getSize(word, font);
+    width = sizeWord.width;
+    lenght = sizeWord.lenght;
+    for (int i = x; i < x + lenght; i++){
+        for (int j = y; j < y + width; j++){
+            freePlase[i][j] = true;
+        }
+    }
 }
 
 QSize WordCloud::getSize(const std::string& word, const QFont& font){
@@ -21,7 +47,7 @@ QSize WordCloud::getSize(const std::string& word, const QFont& font){
 }
 
 void WordCloud::drawFirstWord(){
-
+    QSize size = getSize(words[0], )
 }
 
 void WordCloud::drawWord(){
